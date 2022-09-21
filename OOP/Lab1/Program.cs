929abdb7e4b9355b72d479d3079d7d23a6c25086
ValentynMysan04 <valentin.mysan@gmail.com>
2022-09-21 17:26:32 +0300

    second commit

---
+++ OOP/Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("ПІБ:            Мисан В.В.");
+            Console.WriteLine("Факультет:      Факультет інформаційних технологій");
+            Console.WriteLine("Форма навчання: Денна");
+            Console.WriteLine("Група:          3");
+            Console.Read();
+        }
+    }
+}
